b8fb270d9a87c78f38b3abb4c06834df142b3ae7
Simon Bäumer <simon.baeumer@sva.de>
2019-03-07 22:44:57 +0100

    Refactor test command

--- cmd/commander/commander.go
+++ cmd/commander/commander.go
@@ -41,43 +41,44 @@ func main() {
 			Name:      "test",
 			Usage:     "Execute the test suite",
 			ArgsUsage: "[file] [test]",
-			Action: func(c *cli.Context) {
-				file := CommanderFile
-				if c.Args().First() != "" {
-					file = c.Args().First()
-				}
-				fmt.Println("Starting test file " + file + "...")
-				fmt.Println("")
-
-				content, err := ioutil.ReadFile(file)
-				if err != nil {
-					fmt.Println("Error " + err.Error())
-					os.Exit(1)
-				}
-
-				var s suite.Suite
-				s = suite.ParseYAML(content)
-
-				tests := s.GetTests()
-				// Filter tests if test title was given
-				if title := c.Args().Get(1); title != "" {
-					test, err := s.GetTestByTitle(title)
-					if err != nil {
-						log.Fatal(err.Error())
-						os.Exit(1)
-					}
-					tests = []runtime.TestCase{test}
-				}
-
-				results := runtime.Start(tests)
-				if !commander.Start(results) {
-					os.Exit(1)
-				}
-			},
+			Action: testCommand,
 		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func testCommand(c *cli.Context) {
+	file := CommanderFile
+	if c.Args().First() != "" {
+		file = c.Args().First()
+	}
+
+	fmt.Println("Starting test file " + file + "...")
+	fmt.Println("")
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("Error " + err.Error())
+		os.Exit(1)
+	}
+
+	var s suite.Suite
+	s = suite.ParseYAML(content)
+	tests := s.GetTests()
+	// Filter tests if test title was given
+	if title := c.Args().Get(1); title != "" {
+		test, err := s.GetTestByTitle(title)
+		if err != nil {
+			log.Fatal(err.Error())
+			os.Exit(1)
+		}
+		tests = []runtime.TestCase{test}
+	}
+
+	results := runtime.Start(tests)
+	if !commander.Start(results) {
+		os.Exit(1)
 	}
 }
