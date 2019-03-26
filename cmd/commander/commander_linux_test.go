0bf3ffa2928e514af17589dde66b612569ba1bc1
Simon Bäumer <simon.baeumer@sva.de>
2019-03-26 10:25:42 +0100

    Fix fragile windows tests

--- cmd/commander/commander_linux_test.go
+++ cmd/commander/commander_linux_test.go
@@ -6,16 +6,33 @@ import (
 	"testing"
 )
 
+const TestSuiteFile = "/tmp/commander_test.yaml"
+
 func Test_CommanderFile(t *testing.T) {
 	tests := []byte(`
 tests:
     echo hello:
         exit-code: 0
 `)
-	err := ioutil.WriteFile("/tmp/commander_test.yaml", tests, 0755)
+	err := ioutil.WriteFile(TestSuiteFile, tests, 0755)
 
 	assert.Nil(t, err)
 
-	got := testCommand("/tmp/commander_test.yaml", "")
+	got := testCommand(TestSuiteFile, "")
 	assert.Nil(t, got)
+}
+
+func Test_FailingSuite(t *testing.T) {
+	tests := []byte(`
+tests:
+    echo hello:
+        exit-code: 1
+`)
+	err := ioutil.WriteFile(TestSuiteFile, tests, 0755)
+
+	assert.Nil(t, err)
+
+	got := testCommand(TestSuiteFile, "")
+	assert.Equal(t, "Test suite failed", got.Error())
+
 }
