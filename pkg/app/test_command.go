f84eb5f33c7214dde66de25bdca34a12f976eb07
Dylan <dylan.hitt1@gmail.com>
2020-06-16 17:33:40 -0400

    remove uneeded code

--- pkg/app/test_command.go
+++ pkg/app/test_command.go
@@ -12,6 +12,8 @@ import (
 	"github.com/SimonBaeumer/commander/pkg/suite"
 )
 
+var out output.OutputWriter
+
 // TestCommand executes the test argument
 // testPath is the path to the test suite config, it can be a dir or file
 // titleFilterTitle is the title of test which should be executed, if empty it will execute all tests
@@ -21,6 +23,8 @@ func TestCommand(testPath string, testFilterTitle string, ctx AddCommandContext)
 	if ctx.Verbose == true {
 		log.SetOutput(os.Stdout)
 	}
+
+	out = output.NewCliOutput(!ctx.NoColor)
 
 	if testPath == "" {
 		testPath = CommanderFile
@@ -45,7 +49,6 @@ func TestCommand(testPath string, testFilterTitle string, ctx AddCommandContext)
 		return fmt.Errorf(err.Error())
 	}
 
-	out := output.NewCliOutput(!ctx.NoColor)
 	if !out.PrintSummary(result) {
 		return fmt.Errorf("Test suite failed, use --verbose for more detailed output")
 	}
@@ -62,7 +65,6 @@ func testDir(directory string) (output.Result, error) {
 	}
 
 	for _, f := range files {
-		// Skip reading dirs for now. Should we also check valid file types?
 		if f.IsDir() {
 			continue
 		}
@@ -72,7 +74,6 @@ func testDir(directory string) (output.Result, error) {
 			return result, fmt.Errorf("Error " + err.Error())
 		}
 
-		//Can either pass in f.Name() or append to content and unmarshal in yaml
 		s := suite.ParseYAML(content, f.Name())
 		result, err = execute(s, "") //TODO: aggregate into one result
 		if err != nil {
@@ -84,38 +85,33 @@ func testDir(directory string) (output.Result, error) {
 }
 
 func testFile(filePath string, title string) (output.Result, error) {
-	result := output.Result{}
-
 	content, err := readFile(filePath)
 	if err != nil {
-		return result, fmt.Errorf("Error " + err.Error())
+		return output.Result{}, fmt.Errorf("Error " + err.Error())
 	}
 
 	s := suite.ParseYAML(content, "")
-	result, err = execute(s, title)
+	result, err := execute(s, title)
 	if err != nil {
-		return result, fmt.Errorf("Error " + err.Error())
+		return output.Result{}, fmt.Errorf("Error " + err.Error())
 	}
 
 	return result, nil
 }
 
 func execute(s suite.Suite, title string) (output.Result, error) {
-	result := output.Result{}
-
 	tests := s.GetTests()
 	// Filter tests if test title was given
 	if title != "" {
 		test, err := s.GetTestByTitle(title)
 		if err != nil {
-			return result, err
+			return output.Result{}, err
 		}
 		tests = []runtime.TestCase{test}
 	}
 
-	out := output.NewCliOutput(true)
 	r := runtime.NewRuntime(&out, s.Nodes...)
-	result = r.Start(tests)
+	result := r.Start(tests)
 
 	return result, nil
 }
